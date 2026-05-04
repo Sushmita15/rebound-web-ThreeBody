71221e473cd1dc564108dedfdea7603134459896
Sushmita15 <sushmitamandal100@gmail.com>
2026-05-04 14:10:14 -0400

    error: won't let u update x and vy

--- app.py
+++ app.py
@@ -1,39 +1,60 @@
 from flask import Flask, render_template, request
 import rebound
+import time
+import threading
 
+sim = None
 app = Flask(__name__, template_folder='templates')
 
-sim = rebound.Simulation()
-sim.integrator = "ias15"
+def create_sim(x, vy):
+    sim = rebound.Simulation()
+    sim.integrator = "ias15"
 
-print("Creating REBOUND system...")
+    sim.collision = "direct"
+    sim.collision_resolve = "merge"
 
-sim.add(m=1.0)
-sim.add(m=0.01, a=1.0)
-sim.add(m=0, x=0.8, vy=0.5)
+    sim.add(m=1.0)
+    sim.add(m=0.01, a=1.0)
+    sim.add(m=0, x=x, vy=vy)
 
-sim.move_to_com()
+    sim.move_to_com()
+    sim.start_server(port=1234)
 
-sim.start_server(port=1234)
-print("🚀 REBOUND running at http://127.0.0.1:1234")
+    return sim
+
+
+
+def run_simulation():
+    while True:
+        sim.integrate(sim.t + 0.01)
+        time.sleep(0.01)
+
+
+threading.Thread(target=run_simulation, daemon=True).start()
 
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+
 @app.route('/update_sim', methods=['POST'])
 def update_sim():
+    global sim
+
     data = request.json
     x = float(data.get('x'))
     vy = float(data.get('vy'))
 
-    print("Updating particle")
+    print("Restarting simulation with new parameters")
 
-    sim.particles[2].x = x
-    sim.particles[2].vy = vy
+    # stop old simulation
+    if sim is not None:
+        sim.stop_server()
+    sim = create_sim(x, vy)
 
-    return {"status": "updated"}
+    return {"status": "restarted"}
+
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=5001, debug=True, use_reloader=False)
+    app.run(port=5001, debug=True, use_reloader=False)
